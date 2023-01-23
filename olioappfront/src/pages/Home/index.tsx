a8e89d7aa2d394e78812980f423d55f2e7d0369b
Joseph Hirotsu <jhirotsu@gmail.com>
2023-01-23 11:47:51 -0500

    navbar footer files, Home

--- olioappfront/src/pages/Home/index.tsx
+++ olioappfront/src/pages/Home/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import NavBar from '../../CommonComponents/Navbar';
+import Footer from '../../CommonComponents/Footer';
 function index() {
   return (
-    <div>index</div>
+    <div>
+    <NavBar />
+    <Footer />
+    </div>
   )
 }
 
