12ea76d505631aae487e816ff0262f8f34467df2
Joseph Hirotsu <jhirotsu@gmail.com>
2023-01-25 00:16:00 -0500

    checkout form css

--- olioappfront/src/pages/CheckoutPage/CheckoutForm.tsx
+++ olioappfront/src/pages/CheckoutPage/CheckoutForm.tsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function CheckoutForm() {
   return (
-    <div>CheckoutForm</div>
+    <div>
+        <h1 className="text-2xl mb-10">Checkout Page</h1>
+        <div id="billingContainer">
+            <div id="billingFields" className="w-1/2 grid grid-col-1">
+                <label>Full Name</label>
+                <input type="text"></input>
+                <label>Street Address</label>
+                <input type="text"></input>
+                <label>Apt/Unit</label>
+                <input type="text"></input>
+                <label>City</label>
+                <input type="text"></input>
+                <label>State</label>
+                <input type="text"></input>
+                <label>Zip code</label>
+                <input type="text"></input>
+                <label>Phone number</label>
+                <input type="text"></input>
+                <div>
+                    <button className="text-center bg-slate-900 text-white mt-5 py-2 px-10 rounded-md hover:bg-slate-500 text-xl">
+                        Save and Continue
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
 
--- olioappfront/src/pages/CheckoutPage/index.tsx
+++ olioappfront/src/pages/CheckoutPage/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from '../../CommonComponents/Navbar';
+import CheckoutForm from './CheckoutForm';
 
 function CheckoutPage() {
   return (
     <div>
-
+        <NavBar/>
+        <CheckoutForm/>
     </div>
   )
 }
