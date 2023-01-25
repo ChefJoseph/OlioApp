155114d9ff01ed2a1f67098e41ae1e8a52751bd3
Joseph Hirotsu <jhirotsu@gmail.com>
2023-01-24 23:21:16 -0500

    cart, css, user status coditionals

--- olioappfront/src/pages/Cart/CartProductCards.tsx
+++ olioappfront/src/pages/Cart/CartProductCards.tsx
@@ -31,9 +31,9 @@ const handleCartAdd = (product:IShoppingCart) => {
     setShoppingCart(updateCartQuantity);
   };
 
-  const handleRemoveAllItems = () => {
-    setShoppingCart([]);
-  };
+//   const handleRemoveAllItems = () => {
+//     setShoppingCart([]);
+//   };
 
   const handleRemoveSingleItem = (product:IShoppingCart) => {
     const arrayWithoutItem = shoppingCart.filter((cart) => cart.id !== product.id);
@@ -41,34 +41,63 @@ const handleCartAdd = (product:IShoppingCart) => {
   };
 
 const renderCart = shoppingCart.map((product) => (
-    <div id="cartmaprender" key={product.id}>
-        <div>
-            <img 
+    <div id="cartmaprender" key={product.id} className="flex items-center border-b py-3 mx-10">
+{/* Image         */}
+        <div className="flex w-1/6 justify-center" >
+            <img
+            className="w-40 h-40 rounded-md"
             src={product.image_url}
             alt={product.name}
             />
         </div>
-        <div>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
+{/* Name */}
+        <div className="flex w-1/6 justify-center items-center">
+            <p className="w-full text-lg">{product.name}</p>
         </div>
-        <div>
-            <button onClick={() => handleCartSubtract(product)}> - </button>
-            <p>{product.quantity}</p>
-            <button onClick={() => handleCartAdd(product)}> + </button>
+{/* Unit price  */}
+        <div className="flex w-1/6 justify-center items-center">
+            <p className="w-1/6 text-lg">${product.price}</p>
         </div>
-        <div>
+{/* Quantity  */}
+        <div className="flex w-1/6 justify-center items-center">
+            <button 
+            onClick={() => handleCartSubtract(product)}
+            className="text-lg px-4 py-1 border rounded-full bg-zinc-100 hover:bg-zinc-200"
+            > - </button>
+            <p className="text-lg px-3 py-1 mx-3">{product.quantity}</p>
+            <button 
+            className="text-lg px-3 py-1 border rounded-full bg-zinc-100 hover:bg-zinc-200"
+            onClick={() => handleCartAdd(product)}
+            > + </button>
+        </div>
+{/* Total Price  */}
+        <div className="flex w-1/6 justify-center items-center text-lg">
             <p>${product.price && product.quantity ? (product.price * product.quantity) : ''}</p>
         </div>
-        <button onClick = {() => handleRemoveSingleItem(product) }>
-            Remove Item
-        </button>
+{/* remove  */} 
+        <div className="flex w-1/6 justify-center items-center text-lg">
+            <button 
+            className="rounded-lg bg-gray-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            onClick = {() => handleRemoveSingleItem(product) }>
+                Remove
+            </button>
+        </div>
     </div>
 ))
+
   return (
-    <div id="cartcards">
+    <div id="cartcards" className="">
         {renderCart}
-        <button onClick={() => handleRemoveAllItems()}>Clear cart</button>
+{/* Clear Cart  */}
+        {/* <div className="flex justify-end my-5">
+            <button 
+                className="rounded-lg bg-gray-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                onClick={() => handleRemoveAllItems()}
+            >
+                Clear cart
+            </button>
+        </div> */}
+        
     </div>
   )
 }
--- olioappfront/src/pages/Cart/index.tsx
+++ olioappfront/src/pages/Cart/index.tsx
@@ -1,20 +1,86 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NavBar from '../../CommonComponents/Navbar'
 import Footer from '../../CommonComponents/Footer'
 import CartProductCards from './CartProductCards'
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../AuthProvider';
 
 function Cart() {
+  const navigate = useNavigate();
+  const { user, subtotal, cartTotalItems, setShoppingCart } = useContext(AuthContext);
+
+  const handleRemoveAllItems = () => {
+    setShoppingCart([]);
+  };
+
+// eslint-disable-next-line consistent-return
+  const renderCheckOut = () => {
+      if (user.account_type === 'user' && cartTotalItems > 0) {
+      return (
+        <div className="flex justify-end">
+          <div className="grid-col-1 my-5 mr-5">
+  {/* clearcart */}
+            {/* <div >
+              <button 
+                  className="rounded-lg bg-gray-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  onClick={() => handleRemoveAllItems()}
+              >
+                Clear cart
+              </button>
+            </div> */}
+  {/* Subtotal  */}
+            <div className="text-xl pb-2">
+              <p className="">
+                Subtotal
+                <p className="float-right">${subtotal}</p>
+              </p>
+              
+            </div>
+  {/* Checkout btn  */}
+            <div>
+              <button
+                type="button"
+                className=" text-center bg-slate-900 text-white mt-1 py-2 px-10 rounded-md hover:bg-slate-500 text-xl"
+                onClick={() => navigate('/#')}
+              >
+                CheckOut
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+      }
+// If empty cart 
+      if (cartTotalItems === 0) {
+      return (
+        <div className="flex justify-start ml-10 mb-5">
+          <p className="w-full text-3xl">No Items in Cart</p>
+        </div>
+
+      )
+      }
+// If not logged in
+      return (
+          <div className="w-1/6 text-center text-3xl" style={{ marginTop: '-10px' }}>
+          <p className="text-lg">Log in to checkout</p>
+          <button
+              className="bg-slate-900 text-white mt-1 py-2 px-10 rounded-md hover:bg-slate-800 text-xl"
+              onClick={() => { navigate('/login'); }}
+              type="button"
+          >
+              Log In
+          </button>
+          </div>
+      );
+  };
+
   return (
     <div>
         <NavBar/>
-        <h1>Cart</h1>
 {/* cartheaders */}
-        <div id="cartheaders" className="container py-3 mx-10 mt-10">
-            <div id="cartheaders" className="flex justify-around text-4xl  border-b ">
-                <p>Product</p>
-                <p>Unit Price</p>
-                <p>Quantity</p>
-                <p>Price</p>
+        <div id="cartheaders" className="container py-3 mr-4 my-5">
+            <div id="cartheaders" className="text-3xl pl-10 border-b ">
+                <p className="pb-5">Shopping cart</p>
             </div>
         </div>
 {/* renderCartCards         */}
@@ -22,11 +88,11 @@ function Cart() {
            <CartProductCards/>
         </div>
 {/* Totals and Checkout button */}
-        <div className="grid justify-items-end">
-            Total
-            <button>Checkout</button>
+        <div className="w-full">
+            <div className="">
+              {renderCheckOut()}
+            </div>
         </div>
-        
         <Footer/>
     </div>
   )
