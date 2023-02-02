53bc1b1108c90f1635256f29345f5660e4245f73
Joseph Hirotsu <jhirotsu@gmail.com>
2023-02-02 15:50:04 -0500

    slider data

--- olioappfront/src/pages/Home/sliderData.tsx
+++ olioappfront/src/pages/Home/sliderData.tsx
@@ -2,16 +2,16 @@ export const sliderData = [
     {
         h3: "Let the right people know you're open to work",
         h4: "With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.",
-        image: "https://www.tributemedia.com/hubfs/shutterstock_252081805%20(1).webp"
+        image: "https://www.purdueforlife.org/app/uploads/trip_tuscany-banner2.jpg"
     },
     {
         h3: "Conversations today could lead to opportunity tomorrow",
         h4: "Sending messages to people you know is a great way to strengthen relationships as you take the next step in your career.",
-        image: "https://scandiweb.com/blog/wp-content/uploads/2021/09/hero-banner-types-product-based-1024x541.png"
+        image: "https://www.classicescapes.com/wp-content/uploads/2018/05/Tuscany-Italy.-A-lonely-farmhouse-with-cypress-and-olive-trees-rolling-hills_323519972.jpg"
     },
     {
         h3: "Stay up to date on your industry",
         h4: "From live videos, to stories, to newsletters and more, LinkedIn is full of ways to stay up to date on the latest discussions in your industry.",
-        image: "https://scandiweb.com/blog/wp-content/uploads/2021/09/hero-banner-types-product-based-2-1024x422.png"
+        image: "https://www.classicescapes.com/wp-content/uploads/2018/05/Landscapes-of-Tuscany-alley-with-cypresses.jpg"
     }
 ]
